eb1779ba9d3049758ae59e1debb67616a4351f01
aksilont <aksilont@gmail.com>
2022-02-13 16:17:46 +0300

    Initial Commit

---
+++ RickAndMortyGuide/RickAndMortyGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyGuideApp.swift
+//  RickAndMortyGuide
+//
+//  Created by Aksilont on 13.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
